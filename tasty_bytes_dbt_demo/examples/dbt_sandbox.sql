a838d71dd49c35a12dc7d9dff81d201d995e63db
bwk-philip-king <philip.king@brickworks.com.au>
2026-07-24 00:33:48 +0000

    add new file

---
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -0,0 +1,5 @@
+-- replace all references to "tasty_bytes_dbt_db" with "dbt_projects"
+-- replace all references to "tasty_bytes_dbt_wh" with "dbt_projects_wh"
+
+SHOW TABLES IN DATABASE dbt_projects;
+SHOW VIEWS IN DATABASE dbt_projects;

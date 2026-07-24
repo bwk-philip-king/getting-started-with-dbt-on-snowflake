950710b50e7c65bd12e5b7bc1eb8038e74acd518
bwk-philip-king <philip.king@brickworks.com.au>
2026-07-24 01:13:40 +0000

    updated

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -4,3 +4,9 @@
 SHOW TABLES IN DATABASE dbt_projects;
 SHOW VIEWS IN DATABASE dbt_projects;
 SHOW DBT PROJECTS LIKE 'tasty%';
+
+show network policies in account;
+-- Check the user's current network policy:
+SHOW PARAMETERS LIKE 'NETWORK_POLICY' FOR USER github_actions_service_user;
+-- -- Add the new rule:
+-- ALTER NETWORK POLICY BRICKWORKS_SNOWFLAKE_NETWORK_POLICY ADD ALLOWED_NETWORK_RULE_LIST = ('SNOWFLAKE.NETWORK_SECURITY.GITHUBACTIONS_GLOBAL');
